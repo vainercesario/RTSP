ac320edc7d779778c198ecb0b8f2df383740c820
vainercesario <vainer.cesario@portergroup.com.br>
2024-12-26 21:10:44 -0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+# This stage is used when running from VS in fast mode (Default for Debug configuration)
+FROM mcr.microsoft.com/dotnet/runtime:8.0 AS base
+USER root
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y ffmpeg libsdl2-2.0-0 libsdl2-dev \
+    && ffmpeg -version \
+    && mkdir -p /videos && chmod -R 777 /videos
+
+# Criar o diretório de vídeos e garantir permissões
+RUN mkdir -p /videos && chmod -R 777 /videos
+
+# This stage is used to build the service project
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["/RTSP.csproj", "./"]
+RUN dotnet restore "./RTSP.csproj"
+COPY . .
+WORKDIR "/src"
+RUN dotnet build "./RTSP.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+# This stage is used to publish the service project to be copied to the final stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./RTSP.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+# This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+
+RUN chmod -R 777 /videos
+
+ENTRYPOINT ["dotnet", "RTSP.dll"]
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using RtspClientSharp;
+using System.Diagnostics;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Configurações
+        string hostname = "1.1.1.1";
+        string username = "xxxx";
+        string password = "yyyyy";
+        int port = 554; // Porta padrão do RTSP
+
+        var rtspUrl = $"rtsp://{username}:{password}@{hostname}:{port}/cam/realmonitor?channel=1&subtype=1";
+
+        var connectionParameters = new ConnectionParameters(new Uri(rtspUrl));
+
+        var client = new RtspClient(connectionParameters);
+
+        try
+        {
+            Console.WriteLine("Conectando ao stream RTSP...");
+
+            await client.ConnectAsync(CancellationToken.None);
+            Console.WriteLine("Conectado ao stream RTSP!");
+
+            OpenStreamWithFFmpeg(rtspUrl);
+
+            Console.WriteLine("Conexão ativa. Aguardando 10 segundos...");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+
+            Console.WriteLine("Encerrando conexão...");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao conectar: {ex.Message}");
+        }
+        finally
+        {
+            client.Dispose();
+            Console.WriteLine("Conexão encerrada.");
+        }
+    }
+
+    private static void OpenStreamWithFFmpeg(string rtspUrl)
+    {
+        string outputFile = "/videos/output_video.mp4";
+
+        var ffmpegProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/usr/bin/ffmpeg",
+                Arguments = $"-rtsp_transport tcp -probesize 5000000 -analyzeduration 10000000 -i {rtspUrl} -t 15 -vcodec libx264 -c:v libx264 -preset fast -f mp4 {outputFile}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            }
+        };
+
+        // Captura de saída e erro do FFmpeg
+        ffmpegProcess.OutputDataReceived += (sender, e) => Console.WriteLine($"[stdout] {e.Data}");
+        ffmpegProcess.ErrorDataReceived += (sender, e) => Console.WriteLine($"[stderr] {e.Data}");
+
+        try
+        {
+            ffmpegProcess.Start();
+            ffmpegProcess.BeginOutputReadLine();
+            ffmpegProcess.BeginErrorReadLine();
+            ffmpegProcess.WaitForExit();
+            Console.WriteLine("FFmpeg concluído!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao executar FFmpeg: {ex.Message}");
+        }
+    }
+}
